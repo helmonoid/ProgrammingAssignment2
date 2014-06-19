daec2ba11e8c88d577b7b3cc08beb0ea6e683546
Helder Monteiro <heldermonteiro@live.com>
2014-06-19 16:52:26 +0200

    Synch test

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,15 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
 }
 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## If the inverse has already been calculated (and the matrix has not changed), 
+        ## then the cachesolve should retrieve the inverse from the cache.
+        
 }
